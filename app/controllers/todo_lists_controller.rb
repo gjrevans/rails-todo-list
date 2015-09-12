6e6862ab413d13ee21ca367b9df3567320a757ab
Gordon <gjrevans@gmail.com>
2015-09-12 01:28:04 -0400

    Added Devise and Authentication for views

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,70 +1,59 @@
 class TodoListsController < ApplicationController
   before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
-  # GET /todo_lists
-  # GET /todo_lists.json
+
   def index
-    @todo_lists = TodoList.all
+    if user_signed_in?
+      @todo_lists = TodoList.where(user_id: current_user.id)
+    else
+      render :new_user
+    end
   end
 
-  # GET /todo_lists/1
-  # GET /todo_lists/1.json
   def show
   end
 
-  # GET /todo_lists/new
   def new
-    @todo_list = TodoList.new
+    @todo_list = TodoList.new.build
   end
 
-  # GET /todo_lists/1/edit
   def edit
   end
 
-  # POST /todo_lists
-  # POST /todo_lists.json
   def create
     @todo_list = TodoList.new(todo_list_params)
 
-    respond_to do |format|
       if @todo_list.save
-        format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
-        format.json { render :show, status: :created, location: @todo_list }
+        redirect_to @todo_list, notice: 'Todo list was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @todo_list.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
-  # PATCH/PUT /todo_lists/1
-  # PATCH/PUT /todo_lists/1.json
   def update
-    respond_to do |format|
-      if @todo_list.update(todo_list_params)
-        format.html { redirect_to @todo_list, notice: 'Todo list was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo_list }
-      else
-        format.html { render :edit }
-        format.json { render json: @todo_list.errors, status: :unprocessable_entity }
-      end
+    if @todo_list.update(todo_list_params)
+      redirect_to @todo_list, notice: 'Todo list was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /todo_lists/1
-  # DELETE /todo_lists/1.json
   def destroy
     @todo_list.destroy
-    respond_to do |format|
-      format.html { redirect_to root_url, notice: 'Todo list was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_url, notice: 'Todo list was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_list
       @todo_list = TodoList.find(params[:id])
+    end
+
+    def correct_user
+      @todo_list = current_user.todo_lists.find_by(id: params[:id])
+      redirect_to todo_lists_path, notice: "Not Authorized" if @todo_list.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   	root "todo_lists#index"
   		resources :todo_lists do
     	resources :todo_items do
